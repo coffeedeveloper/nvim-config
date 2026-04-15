ec06621a00d355869ac10fe8f1419b6ef6dcbc25
CoffeeDeveloper <coffeedeveloper@gmail.com>
2026-04-15 18:51:58 +0800

    feat: update snacks and bufferline config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,18 +1,28 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    picker = {
-      sources = {
-        explorer = {
-          hidden = true,  -- 显示 . 开头的隐藏文件
-          ignored = true, -- 显示 .gitignore 中的文件
-          exclude = { "node_modules", ".git" },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,  -- 显示 . 开头的隐藏文件
+            ignored = true, -- 显示 .gitignore 中的文件
+            exclude = { "node_modules", ".git" },
+          },
+          files = {
+            hidden = true,
+            ignored = true,
+            exclude = { "node_modules", ".git" },
+          },
         },
-        files = {
-          hidden = true,  -- 搜索 . 开头的隐藏文件
-          ignored = true, -- 搜索 .gitignore 中的文件
-          exclude = { "node_modules", ".git" },
-        },
+      },
+    },
+  },
+  {
+    "akinsho/bufferline.nvim",
+    opts = {
+      options = {
+        always_show_bufferline = true,
       },
     },
   },
